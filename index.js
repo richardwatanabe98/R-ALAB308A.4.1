d9f2f8bb92c9cf60a6e9d4433b5c14dab9468546
Richard Moncion <rmoncion98@gmail.com>
2024-03-04 14:19:41 -0500

    More changes and comments for readability

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ const progressBar = document.getElementById("progressBar");
 // The get favourites button element.
 const getFavouritesBtn = document.getElementById("getFavouritesBtn");
 
+
+//======================== PART 1: API ===============================
 // Step 0: Store your API key here for reference and easy access.
 const API_KEY = "live_Ycsbz69IAUsiIVTbhpcT4WobBLTQ6eFEy68Qu9HMG2L0eZFuKnZpQyxmcPyNCEjE";
+// =====================================================================================
 
+
+
+//// ==================== PART 2: TASKS ==================================================
 // STEP 1: Create an async function "initialLoad" that does the following: 
 
 async function initialLoad () {
@@ -29,15 +35,11 @@ async function initialLoad () {
             
 
          });
-
-    
-    
-
-
-
     } catch(error){
         console.error('Error fetching or processing data:', error);
     }
 }
 
-initialLoad();
+/// function should run immediately in console... check in browser and run the console log command
+
+// ================================================================================
